4e11ed6f95c1e149de448b52188f93a69ffd94ca
Todd Gutschow <Todd Gutschow>
2017-02-26 08:13:48 -0600

    Hello web

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res) {    
+    res.send('hello world!');
+});
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
