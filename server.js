443b843c2c58c4f78d8ba20c3ad97e4ef600b40f
Todd Gutschow <Todd Gutschow>
2017-02-27 16:32:55 -0600

    Get ip, language and os

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var requestIp = require('request-ip');
+
+app.enable('trust proxy');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {    
-    res.send('hello world!');
+    var ipAddress = requestIp.getClientIp(req);
+    var language = req.headers['accept-language'];
+    language = language.split(',')[0];
+    var operatingSystem = req.headers['user-agent'];
+    var platform = operatingSystem.split('(')[1];
+    platform = platform.split(')')[0];
+    res.send({ip: ipAddress, lang: language, os: platform});
 });
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
